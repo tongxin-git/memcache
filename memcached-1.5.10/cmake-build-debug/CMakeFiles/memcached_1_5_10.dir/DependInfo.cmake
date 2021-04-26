d6eded7c382837356b8189dd5a8f6021705d12f6
CORP\zhangtongxin <zhangtongxin@360.cn>
2021-04-26 15:30:19 +0800

    add

---
+++ memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/assoc.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/assoc.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/bipbuffer.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/bipbuffer.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cache.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/cache.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/crawler.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/crawler.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/crc32c.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/crc32c.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/daemon.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/daemon.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/extstore.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/extstore.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/hash.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/hash.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/items.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/items.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/itoa_ljust.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/itoa_ljust.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/jenkins_hash.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/jenkins_hash.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/linux_priv.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/linux_priv.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/logger.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/logger.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/memcached.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/memcached.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/murmur3_hash.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/murmur3_hash.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/openbsd_priv.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/openbsd_priv.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/sasl_defs.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/sasl_defs.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/sizes.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/sizes.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/slab_automove.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/slab_automove.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/slab_automove_extstore.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/slab_automove_extstore.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/slabs.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/slabs.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/solaris_priv.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/solaris_priv.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/stats.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/stats.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/storage.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/storage.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/testapp.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/testapp.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/thread.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/thread.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/timedrun.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/timedrun.c.o"
+  "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/util.c" "/cygdrive/d/gitee/c_open_project/memcached-1.5.10/cmake-build-debug/CMakeFiles/memcached_1_5_10.dir/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
